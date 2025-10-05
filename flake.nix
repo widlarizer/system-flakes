694e01d947eca4f5407c5254676e4395abceb795
Emil J. Tywoniak <emil@tywoniak.eu>
2025-10-05 22:53:38 +0200

    Start hm

--- flake.nix
+++ flake.nix
@@ -2,9 +2,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-vscode-server.url = "github:nix-community/nixos-vscode-server";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... } @ inputs: {
+  outputs = { nixpkgs, home-manager, ... } @ inputs: {
     nixosConfigurations = {
       fridge = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
@@ -17,6 +21,12 @@
           ./modules/desktop-environment.nix
           ./modules/dev.nix
           ./modules/gaming.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.emil = import ./home/common.nix;
+          }
         ];
       };
 
@@ -31,6 +41,12 @@
           ./modules/desktop-environment.nix
           ./modules/dev.nix
           ./modules/gaming.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.emil = import ./home/common.nix;
+          }
         ];
       };
     };
---
+++ home/common.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  home.stateVersion = "25.05";
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+  programs.git = {
+    enable = true;
+    userName  = "Emil J. Tywoniak";
+    userEmail = "emil" + "@" + "tywoniak.eu";
+    aliases = {
+      	lol = "log --graph --oneline --all";
+      	meme = "!f() { current=$(git branch --show-current) && git checkout $1 && git rebase $current && git checkout $current && git merge --ff-only $1; }; f";
+        mememe = "meme";
+    };
+    extraConfig = {
+      core.editor = "nvim";
+      push.autoSetupRemote = true;
+      merge.conflictstyle = "diff3";
+    };
+    delta.enable = true;
+    maintenance.enable = true;
+    maintenance.repositories = [ "/home/emil/pulls/yosys" ];
+  };
+}
