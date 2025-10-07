fd2498ee5778f25b55f8894f751759b349a32092
Emil J. Tywoniak <emil@tywoniak.eu>
2025-10-07 11:31:43 +0200

    hm: default firefox

--- home/common.nix
+++ home/common.nix
@@ -22,5 +22,15 @@
     delta.enable = true;
     maintenance.enable = true;
     maintenance.repositories = [ "/home/emil/pulls/yosys" ];
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "firefox.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+        "x-scheme-handler/about" = "firefox.desktop";
+        "x-scheme-handler/unknown" = "firefox.desktop";
+      };
+    };
   };
 }
