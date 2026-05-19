8fe335783bad6a3d90ddd1ef8dd3592a1dcd23a1
Emil J. Tywoniak <emil@tywoniak.eu>
2026-05-19 14:11:37 +0200

    fixup! fridge: media

--- modules/media-server.nix
+++ modules/media-server.nix
@@ -1,16 +1,19 @@
 { config, pkgs, ... }: {
   environment.systemPackages = with pkgs; [
+    pkgs.jellyfin
+    pkgs.jellyfin-web
+    pkgs.jellyfin-ffmpeg
   ];
   services.plex = {
     enable = true;
     openFirewall = true;
-    user = "plex";
-    group = "plex";
+    user = "emil";
+    group = "users";
   };
   services.jellyfin = {
     enable = true;
     openFirewall = true;
-    user = "plex";
-    group = "plex";
+    user = "emil";
+    group = "users";
   };
 }
